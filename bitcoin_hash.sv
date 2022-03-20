6e1abac722982e62b11ff6f2fd638755bafcf5bb
hassankhairi <56284502+hassankhairi@users.noreply.github.com>
2022-03-19 20:23:07 -0700

    changed var names

--- bitcoin_hash.sv
+++ bitcoin_hash.sv
@@ -9,13 +9,13 @@ parameter num_nonces = 16;
 parameter NUM_OF_WORDS=20;
 
 //list of states
-enum logic [2:0] {IDLE, READ, BLOCK, COMPUTE, MORE_COMPUTE,WRITE} state;
+enum logic [2:0] {IDLE, READ, BLOCK, COMPUTE, MORE_COMPUTE, WRITE} state;
 
 //array to hold the input output values and w values for calculation
-logic [31:0] hout[num_nonces];
+logic [31:0] out[num_nonces];
 logic [31:0] message[19];
-logic [31:0] wxx[16];
-logic [31:0] w[num_nonces][num_nonces];
+logic [31:0] w[16];
+logic [31:0] w_array[num_nonces][num_nonces];
 
 //h0-h7 values and a-h values for word expension and sha operation
 logic [31:0] h0, h1, h2, h3, h4, h5, h6, h7;
@@ -30,9 +30,9 @@ logic cur_we;
 logic [15:0] cur_addr;
 logic [31:0] cur_write_data;
 
-//start_mod and done_mod will be used when inistantiate the SHA-256 modules
-logic done_mod[16];
-logic start_mod;
+//sha_start and sha_done will be used when inistantiate the SHA-256 modules
+logic sha_done[16];
+logic sha_start;
 
 logic [ 7:0] num_blocks;
 
@@ -71,13 +71,13 @@ endfunction
 /*
   sha256_op does the main SHA operations based on the input parameters
 */
-function logic [255:0] sha256_op(input logic [31:0] a, b, c, d, e, f, g, h, ww,
+function logic [255:0] sha256_op(input logic [31:0] a, b, c, d, e, f, g, h, w1,
                                  input logic [7:0] t);
     logic [31:0] S1, S0, ch, maj, t1, t2; // internal signals
 begin
     S1  = rightrotate(e, 6) ^ rightrotate(e, 11) ^ rightrotate(e, 25);
     ch  = (e & f) ^ ((~e) & g);
-    t1  = h + S1 + ch + k[t] + ww;
+    t1  = h + S1 + ch + k[t] + w1;
     S0  = rightrotate(a, 2) ^ rightrotate(a, 13) ^ rightrotate(a, 22);
     maj = (a & b) ^ (a & c) ^ (b & c);
     t2  = S0 + maj;
@@ -95,9 +95,9 @@ endfunction
 */
 function logic [31:0] wtnew; // function with no inputs
   logic [31:0] s0, s1;
-  s0 = rightrotate(wxx[1],7)^rightrotate(wxx[1],18)^(wxx[1]>>3);
-  s1 = rightrotate(wxx[14],17)^rightrotate(wxx[14],19)^(wxx[14]>>10);
-  wtnew = wxx[0] + s0 + wxx[9] + s1;
+  s0 = rightrotate(w[1],7)^rightrotate(w[1],18)^(w[1]>>3);
+  s1 = rightrotate(w[14],17)^rightrotate(w[14],19)^(w[14]>>10);
+  wtnew = w[0] + s0 + w[9] + s1;
 endfunction
 
 
@@ -110,7 +110,7 @@ endfunction
 always_ff @(posedge clk, negedge reset_n) 
 begin 
     if (!reset_n) begin
-        start_mod<=0;
+        sha_start<=0;
         cur_we <= 1'b0;
         state <= IDLE;
     end else case (state) 
@@ -132,16 +132,14 @@ begin
                 f <= 32'h9b05688c;
                 g <= 32'h1f83d9ab;
                 h <= 32'h5be0cd19; 
-//start_mod will be controlling when the sha modules for phase 2 and 3 will begin calculating
-                start_mod<=0;
+//sha_start will be controlling when the sha modules for phase 2 and 3 will begin calculating
+                sha_start<=0;
                 i<=0;
                 j<=0;
                 cur_addr<=message_addr;
                 offset<=0;
                 state<=BLOCK;
-            end else begin
-                state<=IDLE;
-            end 
+            end
         end
 /*
 READ state will read in the the input and store in message
@@ -149,7 +147,7 @@ The first 16 words will also be store in w array to reduce cycles
 */
         READ: begin
             message[offset]<=mem_read_data;
-            wxx[offset]<=mem_read_data;
+            w[offset]<=mem_read_data;
             state<=BLOCK;
             if(offset==NUM_OF_WORDS-2) begin
                 j<=1;
@@ -171,38 +169,38 @@ for phase 2 and 3 calculations
                 //construction of arrays for phase 2 
                 for(int y=0; y<3; y++) begin
                     for(int z=0; z<num_nonces; z++ )begin
-                        w[z][y]<=message[16+y];
+                        w_array[z][y]<=message[16+y];
                     end
                 end
 
-                w[0][3]<=32'd0;
-                w[1][3]<=32'd1;
-                w[2][3]<=32'd2;
-                w[3][3]<=32'd3;
-                w[4][3]<=32'd4;
-                w[5][3]<=32'd5;
-                w[6][3]<=32'd6;
-                w[7][3]<=32'd7;
-                w[8][3]<=32'd8;
-                w[9][3]<=32'd9;
-                w[10][3]<=32'd10;
-                w[11][3]<=32'd11;
-                w[12][3]<=32'd12;
-                w[13][3]<=32'd13;
-                w[14][3]<=32'd14;
-                w[15][3]<=32'd15;
+                w_array[0][3]<=32'd0;
+                w_array[1][3]<=32'd1;
+                w_array[2][3]<=32'd2;
+                w_array[3][3]<=32'd3;
+                w_array[4][3]<=32'd4;
+                w_array[5][3]<=32'd5;
+                w_array[6][3]<=32'd6;
+                w_array[7][3]<=32'd7;
+                w_array[8][3]<=32'd8;
+                w_array[9][3]<=32'd9;
+                w_array[10][3]<=32'd10;
+                w_array[11][3]<=32'd11;
+                w_array[12][3]<=32'd12;
+                w_array[13][3]<=32'd13;
+                w_array[14][3]<=32'd14;
+                w_array[15][3]<=32'd15;
 
                 for(int y=0; y<num_nonces; y++)begin
-                    w[y][4]<=32'h80000000;
+                    w_array[y][4]<=32'h80000000;
                 end
 
                 for(int y=0; y<num_nonces; y++)begin
                     for(int z=5; z<num_nonces-1; z++)begin
-                        w[y][z]<=32'h00000000;
+                        w_array[y][z]<=32'h00000000;
                     end
                 end 
                 for(int y=0; y<num_nonces; y++)begin
-                    w[y][15]<=32'd640;
+                    w_array[y][15]<=32'd640;
                 end
                 offset<=0;
                 state<=COMPUTE;
@@ -217,10 +215,10 @@ for modules which will process phase 2 and 3
             if (i < 64) 
             begin
               for (int n = 0; n < 15; n++) begin
-                wxx[n] <= wxx[n+1]; 
-                wxx[15] <= wtnew();
+                w[n] <= w[n+1]; 
+                w[15] <= wtnew();
               end
-              {a, b, c, d, e, f, g, h} <= sha256_op(a, b, c, d, e, f, g, h, wxx[0], i);
+              {a, b, c, d, e, f, g, h} <= sha256_op(a, b, c, d, e, f, g, h, w[0], i);
               i<=i+1;
               state<=COMPUTE;
             end else 
@@ -241,13 +239,13 @@ MORE_COMPUTE state, here we will start processing the modules and wait for the c
 and when it is finished, then we start preparing for writing and memory addresses
 */
         MORE_COMPUTE: begin
-            start_mod<=1; 
-            if(done_mod[0]!==1)begin 
+            sha_start<=1; 
+            if(sha_done[0]!==1)begin 
                 state<=MORE_COMPUTE;
             end else begin
                 cur_we<=1;
                 cur_addr<=output_addr;
-                cur_write_data<=hout[0];
+                cur_write_data<=out[0];
                 state<=WRITE;    
             end
 
@@ -257,12 +255,12 @@ and when it is finished, then we start preparing for writing and memory addresse
 WRITE state iterated through the 16 values h0[0] to h0[15] and write to the memory
 */
         WRITE: begin
-            if(offset<15)begin
-                cur_write_data<=hout[offset+1];
+            if(offset==16)begin
+					 state<=IDLE;
+            end else begin
+                cur_write_data<=out[offset+1];
                 offset<=offset+1;
                 state<=WRITE;
-            end else begin
-                state<=IDLE;
             end
         end
 
@@ -278,9 +276,9 @@ generate
     for(m=0; m<num_nonces; m=m+1) begin: sha_loop
         sha256_unit sha_inst(
             .clk(clk),
-            .start(start_mod),                
-            .done(done_mod[m]),              
-            .input_message(w[m]),
+            .start(sha_start),                
+            .done(sha_done[m]),              
+            .input_message(w_array[m]),
 				.k(k),
             .reset_n(reset_n),
             .input_hash0(h0),
@@ -291,7 +289,7 @@ generate
             .input_hash5(h5),
             .input_hash6(h6),
             .input_hash7(h7),
-            .output_mod(hout[m]));
+            .output_mod(out[m]));
     end: sha_loop
 endgenerate
 
